fb377a574025053475990878f0a25565aa58f9a4
avvaan <avvaan1981@gmail.com>
2024-09-09 18:25:12 -0400

    update

---
+++ day8/print_fun.py
@@ -0,0 +1,12 @@
+def print_fun():
+    print('hello')
+    print('how are you')
+    print('Buy!')
+print_fun()
+
+def greet_with_a_name(name):
+    print("Hello " + name + "!")
+    print('how are you,'+name)
+
+    print('Buy!'+name)
+greet_with_a_name('John')
